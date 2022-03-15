ca81d599d4be56ee98586429de7432d02c976cca
adithya-girish <girish.adithya111@gmail.com>
2022-03-15 18:09:06 +0530

    Basic Work

--- MyWebApplication/src/main/java/com/application/food/controller/DataController.java
+++ MyWebApplication/src/main/java/com/application/food/controller/DataController.java
@@ -1,15 +1,19 @@
 package com.application.food.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.application.food.models.Product;
 import com.application.food.repository.ProductRepository;
+import com.sipios.springsearch.anotation.SearchSpec;
 
 @RestController
 
@@ -33,10 +37,14 @@ public class DataController {
 		return (List<Product>) product_Repo.findAll();
 	}
 
-//	@GetMapping("/getProductById/{id}")
-//	public Optional<Product> getProductById(@PathVariable("id") int id) {
-//		System.out.println("id: "+id);
-//		return product_Repo.findById(id);
-//	}
+	@GetMapping("/getProductById/{id}")
+	public Optional<Product> getProductById(@PathVariable("id") int id) {
+		return product_Repo.findById(id);
+	}
+
+	@GetMapping("/search/{name}")
+	public Optional<Product> searchByName(@SearchSpec Specification<Product> specs, @PathVariable("name") String name) {
+		return null;
+	}
 
 }
--- MyWebApplication/src/main/java/com/application/food/repository/ProductRepository.java
+++ MyWebApplication/src/main/java/com/application/food/repository/ProductRepository.java
@@ -1,12 +1,11 @@
 package com.application.food.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.application.food.models.Product;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-	
-	
+public interface ProductRepository extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
+
 
 }
- 
